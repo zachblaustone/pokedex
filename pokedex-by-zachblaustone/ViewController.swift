d7af69694b0c7d8bad4c0efc4ebe4cc13889413c
ZachBlaustone <Zachblaustone@gmail.com>
2016-08-05 11:58:10 -0500

    Initial Commit

---
+++ pokedex-by-zachblaustone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-zachblaustone
+//
+//  Created by Zachary Blaustone on 8/5/16.
+//  Copyright © 2016 Pryzm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
